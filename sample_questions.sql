32767ec97e0c019317f8b252f7b10bb74f469108
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2025-08-01 18:16:44 +0530

    Update sample_questions.sql

--- sample_questions.sql
+++ sample_questions.sql
@@ -1,14 +1,219 @@
-Write SQL Query to Display FIRST_NAME with Alias EMPLOYEE_NAME.
-Write SQL Query to Display FIRST_NAME in Upper Case from the Employees Table.
-Write SQL Query to Display Unique DEPARTMENT Values from the Employees Table.
-Write SQL Query to Display the First 3 Chars of FIRST_NAME from the Employees Table.
-Write SQL Query to Find the Position of Alphabet ‘a’ in the FIRST_NAME Column.
-Write SQL Query to Fetch FIRST_NAME from the Employees Table With No White Spaces on the Right.
-Write SQL Query to List DEPARTMENT from the Employees Table With No White Spaces on the Left.
-Write SQL Query to Display Unique DEPARTMENT Values and Their Lengths from the Employees Table.
-Write SQL Query to Replace ‘a’ with ‘A’ in FIRST_NAME from the Employees Table.
-Write SQL Query to Combine FIRST_NAME and LAST_NAME into COMPLETE_NAME.
-Write SQL Query to Print Employees Details Ordered by FIRST_NAME Ascending and DEPARTMENT Descending.
+1) Write SQL Query to Display FIRST_NAME with Alias EMPLOYEE_NAME.
+
+
+SQL> select FIRST_NAME employee_name from employees;
+or 
+ 
+SQL> select FIRST_NAME as employee_name from employees;
+
+EMPLOYEE_NAME
+--------------------
+Ellen
+Sundar
+Mozhe
+David
+Hermann
+Shelli
+=========================================================================================================================================================================================================================
+
+2)Write SQL Query to Display FIRST_NAME in Upper Case from the Employees Table.
+
+ 
+SQL> select upper(FIRST_NAME) employee_name from employees;
+
+EMPLOYEE_NAME
+--------------------
+ELLEN
+SUNDAR
+MOZHE
+DAVID
+HERMANN
+SHELLI
+AMIT
+ELIZABETH
+SARAH
+DAVID
+==========================================================================================================================================================================================
+
+ 
+3)Write SQL Query to Display Unique DEPARTMENT Values from the Employees Table.
+
+ 
+SQL> select unique department_id from employees;
+
+DEPARTMENT_ID
+-------------
+          100
+           30
+
+           90
+           20
+           70
+          110
+           50
+           80
+           40
+           60
+           10
+ ==========================================================================================================================================================================================
+
+ 4)Write SQL Query to Display the First 3 Chars of FIRST_NAME from the Employees Table.  -->> use of substring
+
+ SQL> select substr(first_name, 1, 3) from employees;     --SUBSTR
+
+SUBSTR(FIRST
+------------
+Ell
+Sun
+Moz
+Dav
+Her
+She
+Ami
+Eli
+==========================================================================================================================================================================================
+
+5)Write SQL Query to Find the Position of Alphabet ‘a’ in the FIRST_NAME Column.
+
+ SQL> SELECT INSTR(FIRST_NAME, 'a') FROM EMPLOYEES;   -->>>>>>>>> A  a BOTH ARE DIFFERENT
+
+INSTR(FIRST_NAME,'A')
+---------------------
+                    0
+                    5
+                    0
+                    2
+                    5
+                    0
+                    0
+                    5
+                    2
+                    2
+
+==========================================================================================================================================================================================
+
+ 
+ 
+6)Write SQL Query to Fetch FIRST_NAME from the Employees Table With No White Spaces on the Right.
+ SELECT RTRIM(FIRST_NAME) FROM EMPLOYEES;
+
+
+
+SQL> SELECT RTRIM(FIRST_NAME) FROM EMPLOYEES;
+
+RTRIM(FIRST_NAME)
+--------------------
+Ellen
+Sundar
+Mozhe
+David
+Hermann
+Shelli
+Amit
+Elizabeth
+Sarah
+David
+===============================================================================================
+ 
+7)Write SQL Query to List DEPARTMENT from the Employees Table With No White Spaces on the Left.
+
+
+ 
+SQL> SELECT LTRIM(FIRST_NAME) FROM EMPLOYEES;
+
+RTRIM(FIRST_NAME)
+--------------------
+Ellen
+Sundar
+Mozhe
+David
+Hermann
+Shelli
+Amit
+Elizabeth
+Sarah
+David
+
+ 
+8)Write SQL Query to Display Unique DEPARTMENT Values and Their Lengths from the Employees Table.
+ 
+SQL> SELECT LTRIM(DEPARTMENT_id) FROM EMPLOYEES;
+
+LTRIM(DEPARTMENT_ID)
+----------------------------------------
+90
+90
+90
+60
+60
+60
+60
+60
+100
+100
+100
+100
+100
+
+
+========================================================================================================================================
+ 
+9)Write SQL Query to Replace ‘a’ with ‘A’ in FIRST_NAME from the Employees Table.
+
+ 
+SQL> select REPLACE(first_name, 'a', 'A') from employees;
+
+--------------------
+Ellen
+SundAr
+Mozhe
+DAvid
+HermAnn
+Shelli
+Amit
+ElizAbeth
+
+ but col name is changing so 
+
+
+SQL>     SELECT
+  2        REPLACE(FIRST_NAME, 'a', 'A') AS FIRST_NAME
+  3     FROM
+  4      EMPLOYEES
+  5  /
+
+FIRST_NAME
+--------------------
+Ellen
+SundAr
+Mozhe
+DAvid
+Anthony
+GerAld
+NAnette
+===================================================================================================
+10)Write SQL Query to Combine FIRST_NAME and LAST_NAME into COMPLETE_NAME.
+
+ In Oracle SQL, the double pipe operator (||) serves as the concatenation
+
+ 
+SQL> select First_name || ' '|| Last_name  completename from Employees;
+
+COMPLETENAME
+----------------------------------------------
+Ellen Abel
+Sundar Ande
+Mozhe Atkinson
+David Austin
+Hermann Baer
+Shelli Baida
+Amit Banda
+Elizabeth Bates
+==================================================================================================================================
+ 
+ 
+ 
+11)Write SQL Query to Print Employees Details Ordered by FIRST_NAME Ascending and DEPARTMENT Descending.
 Write SQL Query to Print Employees Details with First Names “Valli” and “Karen”.
 Write SQL Query to Print Employees Details Excluding First Names “Valli” and “Karen”.
 Write SQL Query to Print Employees Details with DEPARTMENT Name as “Admin”.
