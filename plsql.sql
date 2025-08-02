add96db831c28431a7d88d7f8ec975ccf78afaa7
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2025-08-02 14:48:17 +0530

    Update plsql.sql

--- plsql.sql
+++ plsql.sql
@@ -265,8 +265,267 @@ The salary is 24000 department is 90
 1. variable name can be upto 30 char
 2. should start with CHAR 
 
+1. variable name can be upto 30 CHAR
+2. should **start with CHAR** followed by alphanumeric
+3. **no** reserved words
+4. **no space** allowed
+
+===========================================================================================
 
 
+IF STATEMENTS;
+
+IF <condition> THEN 
+	<statement>;
+END IF;
+
+==============================================================================================================
+	
+DECLARE 
+	v_sal emp.salary%TYPE;
+	v_comm emp.commission%TYPE;
+BEGIN 
+	SELECT salary ,commission_pct
+	INTO v_sal, v_comm
+	FROM emp
+	WHERE employee_id = 100;
+
+	IF v_sal > 20000 AND v_comm IS NULL THEN
+		UPDATE emp
+			SET commission_pct = .5
+		WHERE employee_id  = 100;
+
+		DBMS_OUTPUT.PUT_LINE('THE RECORD UPDATED');
+	
+=========================================================================================================================================
+
+
+	                                            IF CONDITION 
+						    ----------------
+
+
+IF <condition> THEN
+   <statement>;
+END IF;
+
+DECLARE
+   v_sal  emp.salary%TYPE;
+   v_comm emp.commission_pct%TYPE;
+BEGIN
+   SELECT salary, commission_pct
+   INTO v_sal, v_comm
+   FROM emp
+   WHERE employee_id = 100;
+
+   IF v_sal > 20000 AND v_comm IS NULL THEN
+      UPDATE emp
+      SET commission_pct = .5
+      WHERE employee_id = 100;
+
+      DBMS_OUTPUT.PUT_LINE('The record updated');
+   END IF;
+
+   COMMIT;
+END;
+/
+----------------------------------------------------------------------------------------------------
+	
+
+SQL> SELECT * FROM EMP;
+
+EMPLOYEE_ID FIRST_NAME           LAST_NAME                 EMAIL              PHONE_NUMBER         HIRE_DATE JOB_ID          SALARY COMMISSION_PCT MANAGER_ID DEPARTMENT_ID
+----------- -------------------- ------------------------- ------------------------- -------------------- --------- ---------- ---------- -------------- ---------- -------------
+        100 Steven               King                      SKING              515.123.4567         17-JUN-03 AD_PRES          24000             .5                       90
+        101 Neena                Kochhar                   NKOCHHAR                   515.123.4568         21-SEP-05 AD_VP            17000                       100            90
+        102 Lex                  De Haan                   LDEHAAN                    515.123.4569         13-JAN-01 AD_VP            17000                       100            90
+        103 Alexander            Hunold                    AHUNOLD                    590.423.4567         03-JAN-06 IT_PROG           9000                       102            60
+        104 Bruce                Ernst                     BERNST                     590.423.4568         21-MAY-07 IT_PROG           6000                       103            60
+        105 David                Austin                    DAUSTIN                    590.423.4569         25-JUN-05 IT_PROG           4800                       103            60
+        106 Valli                Pataballa                 VPATABAL                   590.423.4560         05-FEB-06 IT_PROG           4800                       103            60
+        107 Diana                Lorentz                   DLORENTZ                   590.423.5567         07-FEB-07 IT_PROG           4200                       103            60
+        108 Nancy                Greenberg                 NGREENBE                   515.124.4569         17-AUG-02 FI_MGR           12008                       101           100
+        109 Daniel               Faviet                    DFAVIET                    515.124.4169         16-AUG-02 FI_ACCOUNT   
+
+
+
+=========================================================================================================================================================================================================================
+                                  NESTED IF STATEMENT
+                                 ------------------------
+
+++++++++++++++++++++++=
++++++++++
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+DECLARE
+   v_sal  emp.salary%TYPE;
+   v_dept emp.department_id%TYPE;
+BEGIN
+   SELECT salary, department_id 
+   INTO v_sal, v_dept 
+   FROM emp 
+   WHERE employee_id = 178;
+
+   IF v_sal > 6500 AND v_dept IS NOT NULL THEN
+      UPDATE emp
+      SET commission_pct = .5
+      WHERE employee_id = 178;
+      
+      DBMS_OUTPUT.PUT_LINE('Updated commission');
+   ELSE
+      UPDATE emp
+      SET department_id = 10
+      WHERE employee_id = 178;
+      
+      DBMS_OUTPUT.PUT_LINE('Updated department');
+   END IF;
+
+   COMMIT;
+END;
+/
+
+
+
+
+
+==========================================
+
+DECLARE
+   v_sal  emp.salary%TYPE;
+   v_dept emp.department_id%TYPE;
+BEGIN
+   SELECT salary, department_id 
+   INTO v_sal, v_dept 
+   FROM emp 
+   WHERE employee_id = 178;
+
+   IF v_sal > 6500 AND v_dept IS NOT NULL THEN
+      UPDATE emp
+      SET commission_pct = .5
+      WHERE employee_id = 178;
+
+      DBMS_OUTPUT.PUT_LINE('Updated commission');
+   ELSE
+      UPDATE emp
+      SET department_id = 10
+      WHERE employee_id = 178;
+
+      DBMS_OUTPUT.PUT_LINE('Updated department');
+   END IF;
+
+   COMMIT;
+END;
+/
+
+
+Updated commission
+
+PL/SQL procedure successfully completed.
+
+SQL> select * from emp;
+
+EMPLOYEE_ID FIRST_NAME           LAST_NAME                 EMAIL                     PHONE_NUMBER      HIRE_DATE JOB_ID          SALARY COMMISSION_PCT MANAGER_ID DEPARTMENT_ID
+----------- -------------------- ------------------------- ------------------------- -------------------- --------- ---------- ---------- -------------- ---------- -------------
+        100 Steven               King                      SKING                     515.123.4567      17-JUN-03 AD_PRES          24000             .5                       90
+        101 Neena                Kochhar                   NKOCHHAR                  515.123.4568      21-SEP-05 AD_VP            17000                       100            90
+        102 Lex                  De Haan                   LDEHAAN                   515.123.4569      13-JAN-01 AD_VP            17000                       100            90
+        103 Alexander            Hunold                    AHUNOLD                   590.423.4567      03-JAN-06 IT_PROG           9000                       102            60
+
+
+
+================================================================================================================================================================================================
+
+
+
+
+
+DECLARE
+   v_sal emp.salary%TYPE;
+BEGIN
+   SELECT salary INTO v_sal FROM emp
+   WHERE employee_id = 180;
+
+   IF v_sal BETWEEN 15000 AND 20000 THEN
+      UPDATE emp
+      SET commission_pct = 0.5
+      WHERE employee_id = 180;
+
+      DBMS_OUTPUT.PUT_LINE('Updated commission');
+
+   ELSIF v_sal BETWEEN 10000 AND 15000 THEN
+      UPDATE emp
+      SET commission_pct = 0.3
+      WHERE employee_id = 180;
+
+      DBMS_OUTPUT.PUT_LINE('Updated department');
+
+   ELSIF v_sal < 10000 THEN
+      UPDATE emp
+      SET commission_pct = 0.2
+      WHERE employee_id = 180;
+
+      DBMS_OUTPUT.PUT_LINE('Updated department');
+
+   ELSE
+      DBMS_OUTPUT.PUT_LINE('Not eligible for commission');
+   END IF;
+ commit;
+END;
+/
+
+======================================================================================================================================================
+
+
+                                 third form of if
+				------------------------
+
+DECLARE
+   v_sal emp.salary%TYPE;
+BEGIN
+   SELECT salary INTO v_sal 
+   FROM emp 
+   WHERE employee_id = 180;
+
+   IF v_sal BETWEEN 15000 AND 20000 THEN
+      UPDATE emp
+      SET commission_pct = 0.5
+      WHERE employee_id = 180;
+
+      DBMS_OUTPUT.PUT_LINE('Updated commission with 0.5');
+
+   ELSIF v_sal BETWEEN 10000 AND 14999 THEN
+      UPDATE emp
+      SET commission_pct = 0.3
+      WHERE employee_id = 180;
+
+      DBMS_OUTPUT.PUT_LINE('Updated commission with 0.3');
+
+   ELSIF v_sal < 10000 THEN
+      UPDATE emp
+      SET commission_pct = 0.2
+      WHERE employee_id = 180;
+
+      DBMS_OUTPUT.PUT_LINE('Updated commission with 0.2');
+
+   ELSE
+      DBMS_OUTPUT.PUT_LINE('Not eligible for commission');
+   END IF;
+
+   DBMS_OUTPUT.PUT_LINE('Out of IF statement');
+   COMMIT;
+END;
+/
 
 
 
